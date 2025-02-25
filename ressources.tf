fdc29dd8a2135a449b76c745a865c6d9f5027ff5
DOUHAB Illies <idouhab072@gmail.com>
2025-02-25 12:51:17 +0100

    correction

--- ressources.tf
+++ ressources.tf
@@ -29,25 +29,19 @@ resource "google_service_account" "cloud_run_sa" {
 resource "google_project_iam_binding" "cloud_run_access" {
   project = var.ID
   role    = "roles/storage.objectViewer"
-#  role	  = "roles/storage.objectCreator"
-#  role    = "roles/storage.objectUser"
   members = ["serviceAccount:${google_service_account.cloud_run_sa.email}"]
 }
 
 # Attribution des droits IAM au Service Account
 resource "google_project_iam_binding" "cloud_run_access_2" {
   project = var.ID
-#  role    = ["roles/storage.objectViewer,"roles/storage.objectCreator","roles/storage.objectUser"]
   role   = "roles/storage.objectCreator"
-#  role    = "roles/storage.objectUser"
   members = ["serviceAccount:${google_service_account.cloud_run_sa.email}"]
 }
 
 # Attribution des droits IAM au Service Account
 resource "google_project_iam_binding" "cloud_run_access_3" {
   project = var.ID
-#  role    = ["roles/storage.objectViewer","roles/storage.objectCreator","roles/storage.objectUser"]
-#  role   = "roles/storage.objectCreator"
   role    = "roles/storage.objectUser"
   members = ["serviceAccount:${google_service_account.cloud_run_sa.email}"]
 }
